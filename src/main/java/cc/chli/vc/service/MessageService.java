e57e46e731109b1fe6859f284850eb6b9a9337a2
Jeremy <jeremy.chen@analyticservice.net>
2017-06-29 10:21:57 +0800

    系统初始化

---
+++ src/main/java/cc/chli/vc/service/MessageService.java
@@ -0,0 +1,5 @@
+package cc.chli.vc.service;
+
+public interface MessageService {
+	String getMessage();
+}
---
+++ src/main/java/cc/chli/vc/service/impl/MessageServiceImpl.java
@@ -0,0 +1,12 @@
+package cc.chli.vc.service.impl;
+
+import cc.chli.vc.service.MessageService;
+
+public class MessageServiceImpl implements  MessageService{
+
+	public String getMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
