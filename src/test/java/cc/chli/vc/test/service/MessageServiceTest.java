ef332ebc69cade4424b2d6d5d2aa99ab9838086a
Jeremy <jeremy.chen@analyticservice.net>
2017-07-13 14:10:16 +0800

    针对 开发 测试 产品之间切换

--- src/test/java/cc/chli/vc/test/service/MessageServiceTest.java
+++ src/test/java/cc/chli/vc/test/service/MessageServiceTest.java
@@ -1,11 +1,13 @@
 package cc.chli.vc.test.service;
 
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)  
-@ContextConfiguration(locations={"classpath:spring-dao-test.xml","classpath:spring-service-test.xml"})  
+@ContextConfiguration(locations={"classpath:spring-context.xml"})
+@ActiveProfiles("test")
 public class MessageServiceTest {
-
+	
 }
