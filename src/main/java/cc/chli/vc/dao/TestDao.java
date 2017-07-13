d35ecf9880284a79fd2377e1933eee0294f55503
Jeremy <jeremy.chen@analyticservice.net>
2017-07-13 18:25:20 +0800

    添加mybatis 自动生成

--- src/main/java/cc/chli/vc/dao/TestDao.java
+++
@@ -1,7 +0,0 @@
-package cc.chli.vc.dao;
-
-import cc.chli.vc.dao.impl.ITestDAO;
-
-public class TestDao implements ITestDAO {
-
-}
--- src/main/java/cc/chli/vc/dao/impl/ITestDAO.java
+++
@@ -1,5 +0,0 @@
-package cc.chli.vc.dao.impl;
-
-public interface ITestDAO {
-
-}
