cfb0a0717e5edf9571c4e7091383722f695432ac
Administrator <Administrator@WIN-DJ7NEUN40Q3>
2017-07-23 20:41:22 +0800

    添加页面

---
+++ src/main/java/cc/chli/vc/controller/BaseController.java
@@ -0,0 +1,5 @@
+package cc.chli.vc.controller;
+
+public class BaseController {
+
+}
--- src/main/java/cc/chli/vc/controller/HelloWorldController.java
+++ src/main/java/cc/chli/vc/controller/HelloWorldController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloWorldController {
 
-    @RequestMapping("/")
-    public String helloWorld(Model model) {
-        model.addAttribute("message", "Hello World!");
-        return "helloWorld";
-    }
+	   @RequestMapping("/helloWorld")
+	    public String helloWorld(Model model) {
+	        model.addAttribute("message", "Hello World!");
+	        return "helloWorld";
+	    }
 }
